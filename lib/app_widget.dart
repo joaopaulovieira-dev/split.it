4a8ab2435b7b0c0ed324889b9d1630ae7949a755
João Paulo Vieira <joaopaulovieira.dev@gmail.com>
2021-08-25 20:02:55 -0300

    Refactoring firebase startup

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:splitit/modules/login/login_page.dart';
+import 'package:splitit/modules/splash/splash_page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Split.it',
-      home: LoginPage(),
+      home: SplashPage(),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:splitit/modules/app_widget.dart';
+import 'package:splitit/app_widget.dart';
 
 void main() {
   runApp(AppWidget());
