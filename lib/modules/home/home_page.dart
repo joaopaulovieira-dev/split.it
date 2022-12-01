4fad8d6011d7425f7d2f00f289c40fd40cba630a
João Paulo Vieira <contato@joaopaulovieira.dev>
2022-12-01 09:33:21 -0300

    Exibindo o nome do user na app bar.

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:splitit/modules/login/models/user_model.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final UserModel user =
+        ModalRoute.of(context)!.settings.arguments as UserModel;
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Page'),
+        title: Text(user.name!),
       ),
     );
   }
