2e95cc302df4da55d923358528eafdbe387cb6a6
João Paulo Vieira <contato@joaopaulovieira.dev>
2022-12-01 16:57:45 -0300

    Adicionando o mocktail

--- test/login_controller_test.dart
+++ test/login_controller_test.dart
@@ -1,32 +1,27 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:splitit/modules/login/login_controller.dart';
 import 'package:splitit/modules/login/login_service.dart';
 import 'package:splitit/modules/login/login_state.dart';
 import 'package:splitit/modules/login/models/user_model.dart';
 
-class LoginServiceMock implements LoginService {
-  @override
-  Future<UserModel> googleSignIn() async {
-    return UserModel(
-      email: 'joaopaulovieira.dev@gmail.com',
-      id: 'id',
-    );
-  }
-}
+class LoginServiceMock extends Mock implements LoginService {}
 
 void main() {
   late LoginController controller;
+  late LoginService service;
+
   setUp(() {
-    controller = LoginController(
-      service: LoginServiceMock(),
-      onUpdate: () {},
-    );
+    service = LoginServiceMock();
+    controller = LoginController(service: service, onUpdate: () {});
   });
 
   test('Testando o Google SingIn retornando sucesso.', () async {
     expect(controller.state, isInstanceOf<LoginStateEmpty>());
     final states = <LoginState>[];
     controller.listen((state) => states.add(state));
+    when(service.googleSignIn)
+        .thenAnswer((_) async => UserModel(email: "email", id: "id"));
     await controller.googleSignIn();
     expect(states[0], isInstanceOf<LoginStateLoading>());
     expect(states[1], isInstanceOf<LoginStateSuccess>());
@@ -37,9 +32,25 @@ void main() {
     expect(controller.state, isInstanceOf<LoginStateEmpty>());
     final states = <LoginState>[];
     controller.listen((state) => states.add(state));
+    when(service.googleSignIn).thenThrow("Erro");
     await controller.googleSignIn();
     expect(states[0], isInstanceOf<LoginStateLoading>());
     expect(states[1], isInstanceOf<LoginStateFailure>());
+    expect((states[1] as LoginStateFailure).message, "Erro");
     expect(states.length, 2);
+  });
+
+  test('Testando o metodo listen', () async {
+    controller.state = LoginStateLoading();
+    controller
+        .listen((state) => expect(state, isInstanceOf<LoginStateLoading>()));
+    controller.update();
+  });
+
+  test('Testando o metodo update', () async {
+    controller.state = LoginStateLoading();
+    controller
+        .listen((state) => expect(state, isInstanceOf<LoginStateLoading>()));
+    controller.update();
   });
 }
